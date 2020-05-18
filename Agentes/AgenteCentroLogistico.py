5db776f32995f5bdfeeb6e518d0270e64f9610c3
ceraalex99 <ceraalex99@gmail.com>
2020-05-18 21:42:01 +0200

    Commit inicial

---
+++ Agentes/AgenteCentroLogistico.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 15:58:13 2013
+
+Esqueleto de agente usando los servicios web de Flask
+
+/comm es la entrada para la recepcion de mensajes del agente
+/Stop es la entrada que para el agente
+
+Tiene una funcion AgentBehavior1 que se lanza como un thread concurrente
+
+Asume que el agente de registro esta en el puerto 9000
+
+@author: Alex
+"""
+
+from multiprocessing import Process, Queue
+import socket
+
+from rdflib import Namespace, Graph, Literal
+from flask import Flask
+from AgentUtil.ACLMessages import get_message_properties, build_message, send_message
+
+from AgentUtil.FlaskServer import shutdown_server
+from AgentUtil.Agent import Agent
+from AgentUtil.Logging import config_logger
+from AgentUtil.OntoNamespaces import DSO
+from rdflib.namespace import RDF, FOAF
+
+__author__ = 'Alex'
+
+
+# Configuration stuff
+hostname = socket.gethostname()
+port = 9031
+
+logger = config_logger(level=1)
+
+agn = Namespace("http://www.agentes.org#")
+
+# Contador de mensajes
+mss_cnt = 0
+
+# Datos del Agente
+
+AgenteCentroLogistico = Agent('AgenteCentroLogistico',
+                       agn.AgenteCentroLogistico,
+                       'http://%s:%d/comm' % (hostname, port),
+                       'http://%s:%d/Stop' % (hostname, port))
+
+# Directory agent address
+DirectoryAgent = Agent('AgenteDirectorio',
+                       agn.Directorio,
+                       'http://%s:9000/Register' % hostname,
+                       'http://%s:9000/Stop' % hostname)
+
+
+# Global triplestore graph
+dsgraph = Graph()
+
+cola1 = Queue()
+
+# Flask stuff
+app = Flask(__name__)
+
+def register():
+    logger.info("Nos registramos")
+    gmess = Graph()
+    gmess.bind('foaf', FOAF)
+    gmess.bind('dso', DSO)
+    reg_obj = agn[AgenteCentroLogistico.name + '-Register']
+    gmess.add((reg_obj, RDF.type, DSO.Register))
+    gmess.add((reg_obj, DSO.Uri, AgenteCentroLogistico.uri))
+    gmess.add((reg_obj, FOAF.name, Literal(AgenteCentroLogistico.name)))
+    gmess.add((reg_obj, DSO.Address, Literal(AgenteCentroLogistico.address)))
+    gmess.add((reg_obj, DSO.AgentType, DSO.AgenteCentroLogistico))
+    # a medias
+
+
+@app.route("/comm")
+def comunicacion():
+    """
+    Entrypoint de comunicacion
+    """
+    global dsgraph
+    global mss_cnt
+    pass
+
+
+@app.route("/Stop")
+def stop():
+    """
+    Entrypoint que para el agente
+
+    :return:
+    """
+    tidyup()
+    shutdown_server()
+    return "Parando Servidor"
+
+
+def tidyup():
+    """
+    Acciones previas a parar el agente
+
+    """
+    pass
+
+
+def agentbehavior1(cola):
+    """
+    Un comportamiento del agente
+
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    # Ponemos en marcha los behaviors
+    ab1 = Process(target=agentbehavior1, args=(cola1,))
+
+    # Ponemos en marcha el servidor
+    app.run(host=hostname, port=port)
+
+    # Esperamos a que acaben los behaviors
+    ab1.join()
+    print('The End')
+
+
